b4f7a03f7a68a862c4e5354335cbed8e224284f0
Cami <cameliabrumar@gmail.com>
2018-11-25 21:57:29 -0500

    some javadoc added

--- zutopia/src/Ball.java
+++ zutopia/src/Ball.java
@@ -16,24 +16,19 @@ public class Ball {
 	/**
 	 * The initial velocity of the ball in the x direction.
 	 */
-	public static final double INITIAL_VX = 3 * 1e-7;
+	public static final double INITIAL_VX = 4* 1e-7;
 	/**
 	 * The initial velocity of the ball in the y direction.
 	 */
-	public static final double INITIAL_VY = 3 * 1e-7;
+	public static final double INITIAL_VY = 4* 1e-7;
 
 	// Instance variables
 	// (x,y) is the position of the center of the ball.
 	private double x, y;
 	private double vx, vy;
 	private Circle circle;
-	private boolean vxChangedLastTime;
-	private boolean vyChangedLastTime;
-	private int counter; // TODO: 24/11/2018 eliminate this variable . only for prints.
-	//private double d; // distance between the points
-	private double xCol, yCol; //x and y of the last collision
-	private String pastColWall;
-	private int nCollisionsBottomWall;
+	private String pastColWall; // the wall that the ball has previously collisioned with.
+	private int nCollisionsBottomWall; // the number of collisions with the lower wall.
 
 	/**
 	 * @return the Circle object that represents the ball on the game board.
@@ -57,14 +52,8 @@ public class Ball {
 		circle.setLayoutY(y - BALL_RADIUS);
 		circle.setFill(Color.BLACK);
 
-		vxChangedLastTime = false;
-		vyChangedLastTime = false;
-		counter = 0;
-		//d = BALL_RADIUS;
-		xCol = -1;
-		yCol = -1;
 		pastColWall = "";
-		nCollisionsBottomWall = 0;
+		nCollisionsBottomWall = 0; // initially the number of times that the ball hit the lower wall is zero.
 	}
 
 	/**
@@ -72,6 +61,9 @@ public class Ball {
 	 * based on the specified elapsed time since the last update.
 	 *
 	 * @param deltaNanoTime the number of nanoseconds that have transpired since the last update
+	 * @param paddle the paddle that is in the game.
+	 * @return the state of the game. If the ball hit the lower wall 5 times then we return GameState.LOST. Otherwise
+	 * 			we return GameState.ACTIVE.
 	 */
 	public GameImpl.GameState updatePosition(long deltaNanoTime, Paddle paddle) {
 
@@ -103,26 +95,13 @@ public class Ball {
 
 	}
 
+	/**
+	 * Changes the velocity, the vertical, the horizontal or both, with the negated velocity, depending on which
+	 * wall did the ball hit or if it hit the paddle..
+	 *
+	 * @param wall the wall/paddle that the ball has hit.
+	 */
 	private void changeVelocity(String wall) {
-		/*if (wall.equals("V")) {
-			if (!vxChangedLastTime) {
-				vx = -vx;
-				vxChangedLastTime = true;
-			} else {
-				vxChangedLastTime = false;
-			}
-		} else if (wall.equals("H")) {
-			if (!vyChangedLastTime) {
-				vy = -vy;
-				vyChangedLastTime = true;
-			} else {
-				vyChangedLastTime = false;
-			}
-		} else { // wall.equals("VH")
-			vx = -vx;
-			vy = -vy;
-		}*/
-
 
 		if (wall.equals("R") || wall.equals("L") ) { // if there is a collision with the right or left wall
 			vx = -vx;
@@ -134,14 +113,17 @@ public class Ball {
 			vx = -vx;
 			vy = -vy;
 		}
+
 	}
 
+	/**
+	 * Determines whether there is a collision with the walls and/or the paddle and returns them in a string.
+	 *
+	 * @param paddle the paddle in this game.
+	 * @return the collision wall(s) and/or paddle or an empty string if there is no collision.
+	 */
 	private String collisionWallOrPaddle(Paddle paddle) {
 		String collision = "";
-
-		double topPady = paddle.getY() - paddle.PADDLE_HEIGHT/2;
-		double bottomPady = paddle.getY() + paddle.PADDLE_HEIGHT/2;
-
 
 		//corners of the paddle
 		double ballBLx = x - BALL_RADIUS; // bottom left
@@ -156,39 +138,29 @@ public class Ball {
 		double ballURx = x + BALL_RADIUS; // upper right
 		double ballURy = y - BALL_RADIUS;
 
-		if (pointIsInThePaddle(ballBLx, ballBLy, paddle)
+		if (pointIsInThePaddle(ballBLx, ballBLy, paddle) // if one of the corners of the box that surrounds the ball is inside the paddle rectangle.
 				|| pointIsInThePaddle(ballBRx, ballBRy, paddle)
 				|| pointIsInThePaddle(ballULx, ballULy, paddle)
 				|| pointIsInThePaddle(ballURx, ballURy, paddle)) {
 			collision += "P";
 		}
 
-		/*if ((x <= (paddle.getX() + paddle.PADDLE_WIDTH / 2))
-				&& (x >= (paddle.getX() - paddle.PADDLE_WIDTH / 2))) {
-			//System.out.println("paddle?");
-
-			if ((y + BALL_RADIUS <= (paddle.getY() - paddle.PADDLE_HEIGHT / 2))
-					&& (y + BALL_RADIUS >= (paddle.getY() + paddle.PADDLE_HEIGHT / 2))) {
-				System.out.println("T");
-				collision += "T";
-			} else if ((y - BALL_RADIUS <= (paddle.getY() + paddle.PADDLE_HEIGHT / 2))
-					&& (y - BALL_RADIUS >= (paddle.getY() - paddle.PADDLE_HEIGHT / 2))) {
-				System.out.println("B");
-				collision += "B";
-			}
-		}*/
-
-		if (x + BALL_RADIUS >= GameImpl.WIDTH) { // right wall
+		if (x + BALL_RADIUS >= GameImpl.WIDTH) { // if the ball hit the right wall
 			collision += "R";
-		} else if (x - BALL_RADIUS <= 0) { // left wall
+		} else if (x - BALL_RADIUS <= 0) { // if the ball hit the left wall
 			collision += "L";
 		}
 
-		if (y + BALL_RADIUS >= GameImpl.HEIGHT) { // upper wall
+		if (y + BALL_RADIUS >= GameImpl.HEIGHT) { // if the ball hit the lower wall
 			collision += "D";
-		} else if (y - BALL_RADIUS <= 0) { // down wall
+		} else if (y - BALL_RADIUS <= 0) { // if the ball hit the upper wall
 			collision += "U";
 		}
+
+		// NOTE: it is impossible that the ball hits the right and the left walls at the same time, that's why
+		// I used if+else. The same thing happens with the upper and the lower wall. However it is possible for
+		// the ball to hit the lower and the right wall simultaneously, which means that the ball has just hit
+		// lower-left corner of the game pane.
 
 		return collision;
 	}
@@ -201,3 +173,5 @@ public class Ball {
 	}
 
 }
+
+// TODO: 25/11/2018 remove system.outs!
