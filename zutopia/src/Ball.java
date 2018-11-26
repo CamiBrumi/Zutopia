80a3966327300cb3499a71309f36ff274a71c14c
Cami <cameliabrumar@gmail.com>
2018-11-25 19:55:43 -0500

    now the collision with the walls and the paddle work perfectly:). Now I have to implement the fact that if the ball touches the bottom wall 5 times consecutivelly, the player will lose the game.

--- zutopia/src/Ball.java
+++ zutopia/src/Ball.java
@@ -16,11 +16,11 @@ public class Ball {
 	/**
 	 * The initial velocity of the ball in the x direction.
 	 */
-	public static final double INITIAL_VX = 4*1e-7;
+	public static final double INITIAL_VX = 3 * 1e-7;
 	/**
 	 * The initial velocity of the ball in the y direction.
 	 */
-	public static final double INITIAL_VY = 4*1e-7;
+	public static final double INITIAL_VY = 3 * 1e-7;
 
 	// Instance variables
 	// (x,y) is the position of the center of the ball.
@@ -37,7 +37,7 @@ public class Ball {
 	/**
 	 * @return the Circle object that represents the ball on the game board.
 	 */
-	public Circle getCircle () {
+	public Circle getCircle() {
 		return circle;
 	}
 
@@ -45,9 +45,9 @@ public class Ball {
 	 * Constructs a new Ball object at the centroid of the game board
 	 * with a default velocity that points down and right.
 	 */
-	public Ball () {
-		x = GameImpl.WIDTH/2;
-		y = GameImpl.HEIGHT/2;
+	public Ball() {
+		x = GameImpl.WIDTH / 2;
+		y = GameImpl.HEIGHT / 2;
 		vx = INITIAL_VX;
 		vy = INITIAL_VY;
 
@@ -68,20 +68,15 @@ public class Ball {
 	/**
 	 * Updates the position of the ball, given its current position and velocity,
 	 * based on the specified elapsed time since the last update.
+	 *
 	 * @param deltaNanoTime the number of nanoseconds that have transpired since the last update
 	 */
-	public void updatePosition (long deltaNanoTime, Paddle paddle) {
-		/*double d = BALL_RADIUS/4.0;
-		if (xCol != -1 && yCol != -1) {
-			final double h = x - xCol;
-			final double v = y - yCol;
-			d = sqrt(h*h + v*v);
-		}*/
+	public void updatePosition(long deltaNanoTime, Paddle paddle) {
+		
 		final String colW = collisionWallOrPaddle(paddle);
-
-
 		if (!colW.equals("")) {
 			if (!colW.equals(pastColWall)) {
+				System.out.println(colW);
 				changeVelocity(colW);
 				pastColWall = colW;
 			}
@@ -120,50 +115,80 @@ public class Ball {
 		}*/
 
 
-		if (wall.equals("R") || wall.equals("L")) { // if there is a collision with the right or left wall
+		if (wall.equals("R") || wall.equals("L") ) { // if there is a collision with the right or left wall
 			vx = -vx;
 
-		} else if (wall.equals("U") || wall.equals("D")) { // if there is a collision with the upper or down wall
+		} else if (wall.equals("U") || wall.equals("D") || wall.equals("P")) { // if there is a collision with the upper down wall, top or bottom of the paddle
 			vy = -vy;
 
-		} else { // wall.equals("RU") or wall.equals("RD") or wall.equals("LU") or wall.equals("LD")
+		} else { // wall.equals(corner), where corner can be a corner formed by two walls or by the paddle and a wall.
 			vx = -vx;
 			vy = -vy;
 		}
 	}
 
 	private String collisionWallOrPaddle(Paddle paddle) {
-	    /*if ((y + BALL_RADIUS <= (paddle.getY() - paddle.PADDLE_HEIGHT/2))
-                && (y + BALL_RADIUS >= (paddle.getY() + paddle.PADDLE_HEIGHT/2))
-                && (x <= (paddle.getX() + paddle.PADDLE_WIDTH/2))
-                && (x >= (paddle.getX() - paddle.PADDLE_WIDTH/2))) { // the bottom of the ball collides with the paddle
-	        System.out.println("DP");
-	        return "D";
-        } else if ((y - BALL_RADIUS <= (paddle.getY() + paddle.PADDLE_HEIGHT/2))
-                && (y - BALL_RADIUS >= (paddle.getY() - paddle.PADDLE_HEIGHT/2))
-                && (x <= (paddle.getX() + paddle.PADDLE_WIDTH/2))
-                && (x >= (paddle.getX() - paddle.PADDLE_WIDTH/2))) { // the top of the ball collides with the paddle
-            System.out.println("UP");
-            return "U";
-        }*/
-	    String collision = "";
+		String collision = "";
+
+		double topPady = paddle.getY() - paddle.PADDLE_HEIGHT/2;
+		double bottomPady = paddle.getY() + paddle.PADDLE_HEIGHT/2;
+
+
+		//corners of the paddle
+		double ballBLx = x - BALL_RADIUS; // bottom left
+		double ballBLy = y + BALL_RADIUS;
+
+		double ballBRx = x + BALL_RADIUS; // bottom right
+		double ballBRy = y + BALL_RADIUS;
+
+		double ballULx = x - BALL_RADIUS; // upper left
+		double ballULy = y - BALL_RADIUS;
+
+		double ballURx = x + BALL_RADIUS; // upper right
+		double ballURy = y - BALL_RADIUS;
+
+		if (pointIsInThePaddle(ballBLx, ballBLy, paddle)
+				|| pointIsInThePaddle(ballBRx, ballBRy, paddle)
+				|| pointIsInThePaddle(ballULx, ballULy, paddle)
+				|| pointIsInThePaddle(ballURx, ballURy, paddle)) {
+			collision += "P";
+		}
+
+		/*if ((x <= (paddle.getX() + paddle.PADDLE_WIDTH / 2))
+				&& (x >= (paddle.getX() - paddle.PADDLE_WIDTH / 2))) {
+			//System.out.println("paddle?");
+
+			if ((y + BALL_RADIUS <= (paddle.getY() - paddle.PADDLE_HEIGHT / 2))
+					&& (y + BALL_RADIUS >= (paddle.getY() + paddle.PADDLE_HEIGHT / 2))) {
+				System.out.println("T");
+				collision += "T";
+			} else if ((y - BALL_RADIUS <= (paddle.getY() + paddle.PADDLE_HEIGHT / 2))
+					&& (y - BALL_RADIUS >= (paddle.getY() - paddle.PADDLE_HEIGHT / 2))) {
+				System.out.println("B");
+				collision += "B";
+			}
+		}*/
+
 		if (x + BALL_RADIUS >= GameImpl.WIDTH) { // right wall
 			collision += "R";
-		}
-		if (x - BALL_RADIUS <= 0) { // left wall
+		} else if (x - BALL_RADIUS <= 0) { // left wall
 			collision += "L";
 		}
+
 		if (y + BALL_RADIUS >= GameImpl.HEIGHT) { // upper wall
+			collision += "D";
+		} else if (y - BALL_RADIUS <= 0) { // down wall
 			collision += "U";
 		}
-		if (y - BALL_RADIUS <= 0) { // down wall
-			collision += "D";
-		}
-		if (!collision.equals("")) {
-			/*xCol = x;
-			yCol = y;*/
-			System.out.println(collision);
-		}
+
 		return collision;
 	}
+
+	private boolean pointIsInThePaddle (double px, double py, Paddle pad) {
+		return ((px >= (pad.getX() - pad.PADDLE_WIDTH/2))
+				&& (px <= (pad.getX() + pad.PADDLE_WIDTH/2))
+				&& (py >= (pad.getY() - pad.PADDLE_HEIGHT))
+				&& (py <= (pad.getY() + pad.PADDLE_HEIGHT)));
+	}
+
 }
